fb3894f59854ac790c4d3e3146e7038ca7ffed4a
KarapetEV <karapet2011@gmail.com>
2019-08-27 17:26:38 +0300

    Parser whith files counter and running-time counter

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,11 +6,13 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Parser {
     private static List<String> projectLinks = new ArrayList<String>();
     private static String baseUrl = "http://fcpir.ru/participation_in_program/contracts/14.598.11.0098?PAGEN_1=";
+    private static int count = 0;
 
     private static Document getPage(String url) throws IOException {
 
@@ -29,20 +31,44 @@ public class Parser {
         Elements tr = tbody.select("tr");
         for (Element row: tr) {
             Element td = row.select("td").first();
-            String projectPage = "http:/" + td.getElementsByTag("a").attr("href");
+            String projectPage = "http://fcpir.ru" + td.getElementsByTag("a").attr("href");
             projectLinks.add(projectPage);
         }
     }
 
+    private static void findFiles() throws IOException {
+        for (String url: projectLinks) {
+            Document page = Jsoup.parse(new URL(url), 3000);
+            Elements aElements = page.select("a[class=panel-some-doc preview]");
+            count += aElements.size();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
         Document page = getPage(baseUrl);
         String url = "";
         Element pagination = page.select("div[class=pagination]").first();
-        for (int i = 1; i <= getNumberOfPages(pagination); i++) {
-            url = baseUrl + String.valueOf(i);
+        int num = getNumberOfPages(pagination);
+        for (int i = 1; i <= num; i++) {
+            url = baseUrl + i;
             page = getPage(url);
             getLinks(page);
+            findFiles();
+            projectLinks.clear();
         }
+        long stop = System.currentTimeMillis();
+        double time = stop - start;
+        System.out.println("Количество файлов: " + count);
+        getTime(time);
+    }
 
+    private static void getTime(double time) {
+
+        int sec = (int) time/1000;
+        int msc = (int) time - sec*1000;
+        int min = sec / 60;
+        sec = sec - min*60;
+        System.out.println("Время выполнения: " + min + " минут " + sec + " секунд " + msc + " миллисекунд");
     }
 }
